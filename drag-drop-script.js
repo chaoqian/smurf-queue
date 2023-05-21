12786e57a348b3465ccebf76729b3daec9b4eb9c
Chao Qian <chqi@microsoft.com>
2023-05-21 11:04:56 -0700

    fix a bug

--- drag-drop-script.js
+++ drag-drop-script.js
@@ -1,12 +1,12 @@
 // Attach event listener to a parent element that remains in the DOM
-document.addEventListener('dragstart', function(event) {
+document.addEventListener('dragstart', function (event) {
   if (event.target.classList.contains('draggable')) {
     // Handle the dragStart event on draggable elements
     event.dataTransfer.setData('text/plain', event.target.id);
   }
 });
 
-document.addEventListener('dragend', function(event) {
+document.addEventListener('dragend', function (event) {
   if (event.target.classList.contains('draggable')) {
     // Handle the dragStart event on draggable elements
     event.preventDefault();
@@ -28,6 +28,14 @@ function drop(event) {
   if (dropzone.classList.contains('elements-wrapper')) {
     // empty
     dropzone.appendChild(element);
+  } else if (dropzone.classList.contains('queue')) {
+    const allChildren = dropzone.children;
+    for (var index = 0; index < allChildren.length; index ++) {
+      if (allChildren[index].classList.contains('elements-wrapper')) {
+        allChildren[index].appendChild(element);
+        break;
+      }
+    }
   } else if (dropzone.parentNode.classList.contains('elements-wrapper')) {
     const siblings = dropzone.parentNode.children;
     const dropIndex = Array.from(siblings).indexOf(dropzone);
@@ -44,3 +52,8 @@ all_elements_wrapper.forEach((element) => {
   element.addEventListener('drop', drop);
 });
 
+const all_queues = document.querySelectorAll('.queue');
+all_queues.forEach((element) => {
+  element.addEventListener('dragover', dragOver);
+  element.addEventListener('drop', drop);
+});
